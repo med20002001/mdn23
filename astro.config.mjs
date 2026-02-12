539075c3335f19a5d993f96ac40f12e3d4e0136e
med20002001 <mohamed1berkaoui@gmail.com>
2026-02-12 14:08:12 +0100

    mdn23 logo sur assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://www.mdn23.com",
   output: "server",
   adapter: cloudflare(),
   integrations: [react(), tailwind()]
